a41ab955a0dbb4602ae646778009e627b6407efa
khufarzad <160780389+khufarzad@users.noreply.github.com>
2024-03-08 10:55:55 +0330

    Other Form For OR Clause

--- Selection.sql
+++ Selection.sql
@@ -127,3 +127,8 @@ WHERE Score > 8 AND Score < 30 AND Age > 21;
 SELECT * FROM Info
 WHERE Age <= 20 OR Score >= 90;
 
+SELECT * FROM Info 
+WHERE Score = 100 OR Score = 97 OR Score = 85;
+
+SELECT * FROM Info
+WHERE First_Name IN ('Zahra', 'Marziye');
